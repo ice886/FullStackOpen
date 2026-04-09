81561b9193856729ba601e3a342cdca1e275177d
ice886 <liuyang20211001@icloud.com>
2026-04-09 16:30:10 +0800

    part2b

--- part2/part2a/App.jsx
+++ part2/part2a/App.jsx
@@ -1,18 +1,50 @@
 import Note from './components/Note'
+import { useState } from 'react'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
 
-const App = ({ notes }) => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      conent: newNote, 
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1)
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
-            <li key={note.id}>
-                <Note note={note} />
-            </li>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </ul>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
