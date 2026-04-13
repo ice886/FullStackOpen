78214f8e04d8aaf253fc8c9db06bceca5cf11b0a
ice886 <liuyang20211001@icloud.com>
2026-04-13 22:02:48 +0800

    part4d

--- Notes/backend/app.js
+++ Notes/backend/app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -27,6 +28,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 
